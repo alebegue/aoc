847149da2c7f1c701db33be2f1b65e813b7b30fd
Adrien Lebègue <adrien.lebegue@gmail.com>
2022-12-09 21:14:39 +0100

    Day 9 Rust solution part 1 updated

--- src/bin/day-9.rs
+++ src/bin/day-9.rs
@@ -1,37 +1,27 @@
 use anyhow::Result;
 use std::collections::HashMap;
 
-#[derive(Eq, Hash, PartialEq)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
 struct Rope {
-    head: Position,
-    tail: Position,
-    history: HashMap<Position, i32>,
+    x: Vec<i32>,
+    y: Vec<i32>,
+    history: HashMap<(i32, i32), i32>,
 }
 
 impl Rope {
     fn move_one_step(&mut self, direction: char) {
-        let tail_position = Position {
-            x: self.tail.x,
-            y: self.tail.y,
-        };
-
+        let tail_position = (self.x.last().unwrap().clone(), self.y.last().unwrap().clone());
         *self.history.entry(tail_position).or_insert(1) += 1;
 
         match direction {
-            'U' => self.head.y += 1,
-            'D' => self.head.y -= 1,
-            'L' => self.head.x -= 1,
-            'R' => self.head.x += 1,
+            'U' => self.y.first_mut().map(|y| *y += 1),
+            'D' => self.y.first_mut().map(|y| *y -= 1),
+            'L' => self.x.first_mut().map(|x| *x -= 1),
+            'R' => self.x.first_mut().map(|x| *x += 1),
             _ => panic!("Invalid direction"),
-        }
+        };
 
-        let dx = self.head.x - self.tail.x;
-        let dy = self.head.y - self.tail.y;
+        let dx = self.x.first().unwrap() - self.x.last().unwrap();
+        let dy = self.y.first().unwrap() - self.y.last().unwrap();
 
         let mut move_x = 0;
         let mut move_y = 0;
@@ -48,8 +38,8 @@ impl Rope {
             move_y = dy - dy.signum();
         }
 
-        self.tail.x += move_x;
-        self.tail.y += move_y;
+        self.x.last_mut().map(|x| *x += move_x);
+        self.y.last_mut().map(|y| *y += move_y);
     }
 }
 
@@ -71,8 +61,8 @@ fn main() -> Result<()> {
     println!("Part 1");
 
     let mut rope = Rope {
-        head: Position { x: 0, y: 0 },
-        tail: Position { x: 0, y: 0 },
+        x: vec![0, 0],
+        y: vec![0, 0],
         history: HashMap::new(),
     };
 
